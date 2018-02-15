2e060abf521af724291143534e0793291a39c412
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-02-15 18:17:57 +1000

    Add app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var package = require('./package.json');
+
+var app = express();
+
+// routes setup
+var index = require('./routes/index');
+
+// global variables
+app.locals.appVersion = package.version;
+app.locals.appAuthor = package.author;
+  
+// middleware setup
+
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// views setup
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
