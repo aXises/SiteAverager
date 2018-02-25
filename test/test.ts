0d35527e5724d0a4f0f3d145c13ba0bf04143f02
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-02-25 21:22:39 +1000

    Test server

---
+++ test/test.ts
@@ -0,0 +1,33 @@
+var origin = 'http://localhost:3000';
+
+import * as request from 'request';
+import * as assert from 'assert';
+import * as mocha from 'mocha';
+import * as http from 'http';
+import * as app from '../app'
+
+describe('App', function () 
+{
+	describe('Server', function ()
+	{
+		before(function (done) 
+		{
+			var server = http.createServer(app);
+			server.listen('3000');
+			done();
+		});
+		it('Server initializes', function (done)
+		{
+			request.get(origin, function (err, res)
+			{
+				if (err) throw err;
+				else assert.equal(200, res.statusCode);
+				done();
+			});
+		});
+		after(function ()
+		{
+			process.exit(0);
+		});
+	});
+});
