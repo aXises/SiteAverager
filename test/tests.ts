8d2bf4f75a934f29b88e775d3e05b693c8c96b5a
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-03-03 01:19:41 +1000

    Added new file to hold test variables

---
+++ test/tests.ts
@@ -0,0 +1,53 @@
+let constBlack = {
+	'RGB': [0, 0, 0],
+	'HEX': '000000',
+	'CMY': [1, 1, 1],
+	'CMYK': [0, 0, 0, 1],
+	'HSL': [0, 0 ,0]
+}
+
+let constWhite = {
+	'RGB': [255, 255, 255],
+	'HEX': 'FFFFFF',
+	'CMY': [0, 0, 0],
+	'CMYK': [0, 0, 0, 0],
+	'HSL': [0, 0 ,0]
+}
+
+let constRed = {
+	'RGB': [255, 0, 0],
+	'HEX': 'FF0000',
+	'CMY': [0, 0, 0],
+	'CMYK': [0, 0, 0, 0],
+	'HSL': [0, 0 ,0]
+}
+
+let constGreen = {
+	'RGB': [0, 255, 0],
+	'HEX': '00FF00',
+	'CMY': [0, 0, 0],
+	'CMYK': [0, 0, 0, 0],
+	'HSL': [0, 0 ,0]
+}
+
+let constBlue = {
+	'RGB': [0, 0, 255],
+	'HEX': '0000FF',
+	'CMY': [0, 0, 0],
+	'CMYK': [0, 0, 0, 0],
+	'HSL': [0, 0 ,0]
+}
+
+var expected = {
+    colourModes: {
+        cases: {
+            BLACK: constBlack,
+            WHITE: constWhite,
+            RED: constRed,
+            GREEN: constGreen,
+            BLUE: constBlue
+        }
+    }
+}
+
+export { expected }
