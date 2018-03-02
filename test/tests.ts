369f764f7b8b59a8ac7b0de9532b127015e6eb43
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-03-03 01:33:42 +1000

    Added correct values for expected values

--- test/tests.ts
+++ test/tests.ts
@@ -3,7 +3,7 @@ let constBlack = {
 	'HEX': '000000',
 	'CMY': [1, 1, 1],
 	'CMYK': [0, 0, 0, 1],
-	'HSL': [0, 0 ,0]
+	'HSL': [0, 0, 0]
 }
 
 let constWhite = {
@@ -11,31 +11,31 @@ let constWhite = {
 	'HEX': 'FFFFFF',
 	'CMY': [0, 0, 0],
 	'CMYK': [0, 0, 0, 0],
-	'HSL': [0, 0 ,0]
+	'HSL': [0, 0, 1]
 }
 
 let constRed = {
 	'RGB': [255, 0, 0],
 	'HEX': 'FF0000',
-	'CMY': [0, 0, 0],
-	'CMYK': [0, 0, 0, 0],
-	'HSL': [0, 0 ,0]
+	'CMY': [0, 1, 1],
+	'CMYK': [0, 1, 1, 0],
+	'HSL': [0, 1, 0.5]
 }
 
 let constGreen = {
 	'RGB': [0, 255, 0],
 	'HEX': '00FF00',
-	'CMY': [0, 0, 0],
-	'CMYK': [0, 0, 0, 0],
-	'HSL': [0, 0 ,0]
+	'CMY': [1, 0, 1],
+	'CMYK': [1, 0, 1, 0],
+	'HSL': [0.33333, 1, 0.5]
 }
 
 let constBlue = {
 	'RGB': [0, 0, 255],
 	'HEX': '0000FF',
-	'CMY': [0, 0, 0],
-	'CMYK': [0, 0, 0, 0],
-	'HSL': [0, 0 ,0]
+	'CMY': [1, 1, 0],
+	'CMYK': [1, 1, 0, 0],
+	'HSL': [0.66667, 1, 0.5]
 }
 
 var expected = {
