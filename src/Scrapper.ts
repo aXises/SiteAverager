5242be276881bbcf368f2f5afe2efbd79a439c16
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-12-20 18:08:42 +1000

    Rework scrapper

--- src/Scrapper.ts
+++ src/Scrapper.ts
@@ -2,106 +2,79 @@ import * as request from "request-promise";
 import * as jsdom from "jsdom";
 import * as jquery from "jquery";
 import * as url from "url";
-import Promise from "promise";
+
+declare const JPEGMagicNumber = "ffd8ff";
+declare const PNGMagicNumber = "89504e";
 
 /** A class to scrape web pages */
 export class Scrapper {
-    private url: string;
-    private images: string[];
+
+    private readonly url: string;
+    private scrappedImages: string[];
 
     /** @constructor
      * @param {string} query - The url of the page to scrape.
      */
     public constructor(query: string) {
         this.url = query;
-        this.images = [];
+        this.scrappedImages = [];
     }
 
     /**
      * Method to scrape the entire page and generate a window.
      */
-    public scrape(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            const self = this;
-            if (self.url.slice(0, 4) !== "http") { self.url = "http://" + self.url; }
-            request({
-                url: self.url,
+    public async scrape(): Promise<void> {
+        try {
+            const result = await request.get({
+                url: this.url,
                 headers: {
-                    gzip: true
-                }
-            }).then((body) => {
-                self.scrapeImg(new jsdom.JSDOM(body).window).then(() => {
-                    resolve(body);
-                }).catch(() => {
-                    reject();
-                });
+                    gzip: true,
+                },
             });
-        });
-
+            if (result.statusCode !== 200) {
+                throw new Error();
+            }
+            this.scrapeImages(new jsdom.JSDOM(result).window);
+        } catch (err) {
+            throw new Error(err);
+        }
     }
 
-    // private loopArr(array: Array<any>, index: number, callback: any): void
-    // {
-    //     let self = this;
-    //     self.vertifiyImg(array[index], function (res)
-    //     {
-    //         console.log("verifiying case", index, array[index])
-    //         // console.log("verifiying case", index + 1)
-    //         // if (res) {
-    //         //     console.log("success, calling back true at", index + 1, array.length)
-    //         //     callback(true, index + 1);
-    //         //     return;
-    //         // }
-    //         // if (!res && index < array.length - 1)
-    //         // {
-    //         //     console.log("invalid, trying next case...")
-    //         //     self.loopArr(array, index + 1, function (res)
-    //         //     {
-    //         //         //callback(false, index + 1);
-    //         //     });
-    //         // }
-    //         // else
-    //         // {
-    //         //     console.log("failed, calling back false", index + 1, array.length)
-    //         //     callback(false);
-    //         //     return;
-    //         // }
-    //     });
-    // }
-
-    private verifyImg(image: string): Promise<any> {
-        return new Promise((resolve, reject) => {
-            request.get({
+    private async verifyImage(image: string): Promise<boolean> {
+        try {
+            const result = await request.get({
                 url: image,
-                encoding: null
-            }).then((body) => {
-                if (body) {
-                    if (body.toString("hex", 0, 3) === "ffd8ff" || body.toString("hex", 0, 3) === "89504e") {
-                        return resolve();
-                    }
-                }
-                reject();
+                encoding: undefined,
             });
-        });
-
+            if (result) {
+                if (result.toString("hex", 0, 3) === JPEGMagicNumber || result.toString("hex", 0, 3) === PNGMagicNumber) {
+                    return true;
+                }
+            }
+            return false;
+        } catch (err) {
+            throw new Error(err);
+        }
     }
 
     /**
      * Method find all images in a window.
      * @param {jsdom.DOMWindow} window - A DOM accessed by jquery.
      */
-    private scrapeImg(window: jsdom.DOMWindow): Promise<any> {
-        return new Promise((resolve, reject) => {
+    private async scrapeImages(window: jsdom.DOMWindow): Promise<void> {
+        try {
             const document = window.document;
             const $: any = jquery(window);
-            $(document).ready(() => {
-                if ($("img").length === 0) { return; }
-                const counter = 0;
-                $("img").each((i) => {
-                    const image = url.resolve(this.url, $(this).attr("src"));
-                    this.verifyImg(image).then().catch();
-                });
+            await $(document).ready();
+            if ($("img").length === 0) { return; }
+            $("img").each(async () => {
+                const image = url.resolve(this.url, $(this).attr("src"));
+                if (await this.verifyImage(image)) {
+                    this.scrappedImages.push(image);
+                }
             });
-        });
+        } catch (err) {
+            throw new Error(err);
+        }
     }
 }
