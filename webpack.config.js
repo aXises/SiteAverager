5d23248f781ba4b9476d69733cf33b7123517102
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-10 20:50:38 +1000

    Fix gitignore ignoring webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const webpack = require("webpack");
+module.exports = {
+    entry: {
+        form: path.resolve(__dirname, "./") + "/public/components/Form.js",
+    },
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "javascripts/[name].bundle.js"
+    },
+    mode: "none",
+}
