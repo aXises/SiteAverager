d6163f19ff7569f32610a81b0f1ee6481979667b
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-10 20:25:01 +1000

    Added validation for empty string

--- public/components/Form.tsx
+++ public/components/Form.tsx
@@ -1,21 +1,36 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-class Form extends React.Component {
+interface IFormFields {
+    value?: string;
+    isValid: boolean;
+}
+
+class Form extends React.Component<{}, IFormFields> {
     public constructor(props) {
         super(props);
+        this.state = {
+            value: "",
+            isValid: false,
+        };
     }
     public render(): React.ReactNode {
         return (
             <form action="analyse" method="POST">
                 <div className="form-group">
-                    <input className="form-control" name="url" type="text" placeholder="https://www.google.com/" />
+                    <input className="form-control" name="url" type="text" placeholder="https://www.google.com/"
+                        value={this.state.value}
+                        onChange={(e) => this.userInput(e)}/>
                 </div>
                 <div className="form-group">
-                    <input className="form-control" type="submit" />
+                    <input className="form-control" type="submit" disabled={!this.state.isValid}/>
                 </div>
             </form>
         );
+    }
+    private userInput(event: React.ChangeEvent): void {
+        const fieldValue = (event.target as HTMLInputElement).value;
+        this.setState({value: fieldValue, isValid: fieldValue.length !== 0});
     }
 }
 
