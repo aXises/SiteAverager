12f17deed227a3abbc773d882968ba819416b53e
Toe, Knee <leletonyli.13944@yahoo.com.au>
2019-01-10 13:50:13 +1000

    Added scrapper tests

--- test/TestScrapper.ts
+++ test/TestScrapper.ts
@@ -1,51 +1,68 @@
 import "mocha";
 import app from "app";
 import Scrapper from "src/Scrapper";
-import * as request from "request";
-import * as assert from "assert";
-import * as mocha from "mocha";
 import { expect } from "chai";
 import * as http from "http";
-import { StatusCodeError } from "request-promise/errors";
+import { StatusCodeError, RequestError } from "request-promise/errors";
+import { testURL } from "test/Test";
+import { fail } from "assert";
+import UnsupportedImageTypeError from "src/exception/UnsupportedImageTypeError";
 
-const server = http.createServer(app);
-const testURL = "http://localhost:3000/sample";
+const imagesURL = "http://localhost:3000/images";
 
 describe("Scrapper", () => {
-    before(() => {
-        server.listen(3000);
-    });
-
-    
-
-    it("Verifies images correctly", async () => {
+    it("Scrapes images correctly.", async () => {
         try {
             const scrapper: Scrapper = new Scrapper(testURL);
-            await scrapper.verifyAndPushImage(testURL + "blue25.png");
-            const imagesURL: string[] = scrapper.getImagesURL();
-            expect(imagesURL.length).to.equal(1);
-            await scrapper.verifyAndPushImage(testURL + "undefinedimage");
-            expect(imagesURL.length).to.equal(1);
-
-        } catch (err) {
-            if (err instanceof StatusCodeError) {
-                expect(err.statusCode).to.equal(404);
-            }
-        }
-    });
-
-    it("Scrapes images correctly", async () => {
-        try {
-            const scrapper: Scrapper = new Scrapper(testURL);
-            const imagesURL: string[] = await scrapper.scrape();
-            expect(imagesURL.length).to.equal(20);
+            const images: string[] = await scrapper.scrape();
+            expect(images).to.be.a("array").of.length(20);
         } catch (err) {
             expect(err).to.be.instanceOf(StatusCodeError);
             expect(err.statusCode).to.equal(404);
         }
     });
-
-    after(() => {
-        server.close();
+    describe("Verifies images correctly.", async () => {
+        it("Verifies a correct JPG file.", async () => {
+            try {
+                const scrapper: Scrapper = new Scrapper(testURL);
+                await scrapper.verifyAndPushImage(imagesURL + "/blue50.jpg");
+                const images: string[] = scrapper.getImagesURL();
+                expect(images).to.be.a("array").of.length(1);
+            } catch (err) {
+                fail();
+            }
+        });
+        it("Verifies a correct PNG file.", async () => {
+            try {
+                const scrapper: Scrapper = new Scrapper(testURL);
+                await scrapper.verifyAndPushImage(imagesURL + "/green25.png");
+                const images: string[] = scrapper.getImagesURL();
+                expect(images).to.be.a("array").of.length(1);
+            } catch (err) {
+                fail();
+            }
+        });
+        it("Verifies a unknown file path.", async () => {
+            try {
+                const scrapper: Scrapper = new Scrapper(testURL);
+                await scrapper.verifyAndPushImage(" ");
+                fail();
+            } catch (err) {
+                if (!(err instanceof RequestError)) {
+                    fail();
+                }
+            }
+        });
+        it("Verifies a corrupt file.", async () => {
+            try {
+                const scrapper: Scrapper = new Scrapper(testURL);
+                await scrapper.verifyAndPushImage(imagesURL + "/red100corrupt.png");
+                fail();
+            } catch (err) {
+                if (!(err instanceof UnsupportedImageTypeError)) {
+                    fail();
+                }
+            }
+        });
     });
 });
